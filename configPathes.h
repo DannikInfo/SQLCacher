0092ccc6a3fae1b8093aba83ada00e8e144265f4
dannikinfo <danil74808@gmail.com>
2024-03-11 21:40:50 +0700

    Remove unnecessary cfg paths

--- configPathes.h
+++ configPathes.h
@@ -1,10 +1,6 @@
 #ifndef SQLCACHE_CONFIGPATHES_H
 #define SQLCACHE_CONFIGPATHES_H
 
-#define DB_URL "dbUrl"
-#define DB_USER "dbUser"
-#define DB_PASS "dbPass"
-#define DB_NAME  "dbName"
 #define DEBUG  "debug"
 #define CACHE_PATHS  "cachePaths"
 #define CLIENT_ID "client.id"
